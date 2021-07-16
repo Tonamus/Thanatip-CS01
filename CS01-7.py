99ae0cd5da85ae88d2b987e14774708c6e902186
Tonnam <Thanatipkhongchob@gmail.com>
2021-07-16 09:27:55 +0700

    Update

---
+++ CS01-7.py
@@ -0,0 +1,10 @@
+Score = int(input('Enter your score: '))
+
+if(Score<=10):
+    print("You failed")
+elif(Score<=20):
+    print("You have to improve")
+elif(Score<=30):
+    print("You did a good job!")
+else:
+    print("What? Try it again")
