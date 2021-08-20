77383c9a1089bdf04e7414414a7b50aa50627897
Tonnam <Thanatipkhongchob@gmail.com>
2021-08-20 09:21:42 +0700

    Update

---
+++ CS01-15.py
@@ -0,0 +1,13 @@
+def my_list(list):
+    for x in list:
+        print(list)
+
+list = [5, 10, 15, 20, 25, 50, 20]
+
+for i in range(len(list)):
+    
+    if(list[i] == 20):
+        list[i] = 200
+print(list)
+
+
