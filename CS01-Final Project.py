b000589013fc001bee438d3cd408eaa6920846e0
Tonnam <Thanatipkhongchob@gmail.com>
2021-10-10 14:05:13 +0700

    update

--- CS01-Final Project.py
+++ CS01-Final Project.py
@@ -76,27 +76,27 @@ if __name__ == "__main__":
                     command=lambda: press(0), height=1, width=7)
     button0.grid(row=5, column=0)
  
-    plus = Button(gui, text=' + ', fg='black', bg='white',
+    plus = Button(gui, text=' + ', fg='black', bg='yellow',
                 command=lambda: press("+"), height=1, width=7)
     plus.grid(row=2, column=3)
  
-    minus = Button(gui, text=' - ', fg='black', bg='white',
+    minus = Button(gui, text=' - ', fg='black', bg='yellow',
                 command=lambda: press("-"), height=1, width=7)
     minus.grid(row=3, column=3)
  
-    multiply = Button(gui, text=' * ', fg='black', bg='white',
+    multiply = Button(gui, text=' * ', fg='black', bg='yellow',
                     command=lambda: press("*"), height=1, width=7)
     multiply.grid(row=4, column=3)
  
-    divide = Button(gui, text=' / ', fg='black', bg='white',
+    divide = Button(gui, text=' / ', fg='black', bg='yellow',
                     command=lambda: press("/"), height=1, width=7)
     divide.grid(row=5, column=3)
  
-    equal = Button(gui, text=' = ', fg='black', bg='white',
+    equal = Button(gui, text=' = ', fg='black', bg='orange',
                 command=equalpress, height=1, width=7)
     equal.grid(row=5, column=2)
  
-    clear = Button(gui, text='Clear', fg='black', bg='white',
+    clear = Button(gui, text='Clear', fg='black', bg='lime',
                 command=clear, height=1, width=7)
     clear.grid(row=5, column='1')
  
